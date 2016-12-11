91a02767a77db43cc732fbc4e2355e49b96e62e8
yeahecho <guoecho@hotmail.com>
2016-12-11 16:50:38 -0500

    init

--- app/src/main/java/com/aiosdev/weather/MainActivity.java
+++ app/src/main/java/com/aiosdev/weather/MainActivity.java
@@ -1,13 +1,22 @@
 package com.aiosdev.weather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bt1;
+    private Button bt2;
+    private Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1 = (Button)findViewById(R.id.bt1);
+        bt2 = (Button)findViewById(R.id.bt2);
+        bt3 = (Button)findViewById(R.id.bt3);
     }
 }
