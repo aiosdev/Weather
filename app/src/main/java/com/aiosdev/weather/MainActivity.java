0dccd030eecd02ede4ce1830b039a848eb8c40bf
suntaoas <suntaoas2014@gmail.com>
2016-12-11 20:40:11 -0500

    add library gson and add activity for function by geographical coordinates.

--- app/src/main/java/com/aiosdev/weather/MainActivity.java
+++ app/src/main/java/com/aiosdev/weather/MainActivity.java
@@ -1,7 +1,9 @@
 package com.aiosdev.weather;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
         bt1 = (Button)findViewById(R.id.bt1);
         bt2 = (Button)findViewById(R.id.bt2);
         bt3 = (Button)findViewById(R.id.bt3);
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, FuncByGeoCoorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
